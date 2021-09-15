3f4a0d857ae27b178ecd208fb9cee21c5407b23b
David Hanacek <d.hanacek@gmx.net>
2021-09-15 19:54:10 +0200

    Delete runtime/docker directory

--- runtime/docker/deploy-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# executed by npm run docker-deploy - see package.json
-
-docker stop emcday2015; 
-docker rm emcday2015; 
-docker build -t mitel/emcday2015 dist/docker/; 
-docker run -d --name=\"emcday2015\" -p 8080:8080 mitel/emcday2015; 
-docker logs -f emcday2015;
--- runtime/docker/prepare-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# prepare the resources needed to build the docker image
-rm -rf dist/docker;
-mkdir dist/docker;
-cp runtime/docker/Dockerfile ./dist/docker;
-cp runtime/docker/supervisord.conf dist/docker;
-cp dist/server/server.js dist/docker;
-cp -r dist/client dist/docker/client
