e455fb2438b1f8d69634f3ed05527a3c433970fa
David Hanacek <d.hanacek@gmx.net>
2021-09-15 19:53:12 +0200

    Delete runtime/cf directory

--- runtime/cf/prepare-cf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# prepare the resources needed to build the docker image
-rm -rf dist/cf;
-mkdir dist/cf;
-cp runtime/cf/package.json dist/cf;
-cp runtime/cf/manifest.yml dist/cf;
-cp -r dist/server/server.js dist/cf;
-cp -r dist/client dist/cf/client;
